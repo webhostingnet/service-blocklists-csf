cc62b85430a77015189d11067811b1ed97e2ac11
Aetherinox <aetherinox@proton.me>
2026-03-26 07:10:57 -0700

    ci: update `url source` for geolite blocklists

--- .github/scripts/bl-geolite2.sh
+++ .github/scripts/bl-geolite2.sh
@@ -2429,7 +2429,7 @@ generate_Continents( )
         curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/descriptions/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/desc.txt &
         curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/categories/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/cat.txt &
         curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/expires/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/exp.txt &
-        curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/url-source/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/src.txt &
+        curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/url-source/geolite2/countries.txt" > ${app_dir_github}/${folder_target_temp}/src.txt &
         wait
 
         # #
@@ -2608,7 +2608,7 @@ generate_Countries( )
         curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/descriptions/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/desc.txt &
         curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/categories/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/cat.txt &
         curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/expires/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/exp.txt &
-        curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/url-source/countries/${templ_id}.txt" > ${app_dir_github}/${folder_target_temp}/src.txt &
+        curl "${templ_curl_opts[@]}" "${app_repo_curl_storage}/url-source/geolite2/countries.txt" > ${app_dir_github}/${folder_target_temp}/src.txt &
         wait
 
         # #
